ee4a31ab605fd7072c0a0c7ffa00adadd465f83d
jipstavenuiter <jipstavenuiter@gmail.com>
2023-08-27 14:46:22 +0800

    initial plasmic code generation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/globals.css'
+import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <PlasmicRootProvider Head={Head}>
+      <Component {...pageProps} />
+    </PlasmicRootProvider>
+  );
 }
