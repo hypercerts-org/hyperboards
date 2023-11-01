7a6ba9cbf496f9d4ffcca4bd4feb1e5beb34a746
jipstavenuiter <jipstavenuiter@gmail.com>
2023-11-01 16:37:41 +0100

    update auth setup

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -7,5 +7,15 @@ export const supabase = createClient<Database>(
 );
 
 export const getSupabaseAuthenticatedClient = (token: string) => {
-  return createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, token);
+  return createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    },
+  );
 };
