88f6c13cbc3e3b2e1ab579bc667432ded3e0ea74
jipstavenuiter <jipstavenuiter@gmail.com>
2024-02-21 12:26:31 +0100

    (chore): format migration

--- supabase/migrations/20240221112005_add_fraction_specific_metadata.sql
+++ supabase/migrations/20240221112005_add_fraction_specific_metadata.sql
@@ -1,24 +1,27 @@
-create table "public"."fraction_sponsor_metadata" (
-                                                      "created_at" timestamp with time zone not null default now(),
-                                                      "type" text not null,
-                                                      "companyName" text,
-                                                      "firstName" text,
-                                                      "lastName" text,
-                                                      "image" text not null,
-                                                      "hypercert_id" text not null,
-                                                      "fraction_id" text not null,
-                                                      "value" text not null,
-                                                      "strategy" text not null,
-                                                      "chain_id" bigint not null,
-                                                      "id" uuid not null default gen_random_uuid()
+create table "public"."fraction_sponsor_metadata"
+(
+    "created_at"   timestamp with time zone not null default now(),
+    "type"         text                     not null,
+    "companyName"  text,
+    "firstName"    text,
+    "lastName"     text,
+    "image"        text                     not null,
+    "hypercert_id" text                     not null,
+    "fraction_id"  text                     not null,
+    "value"        text                     not null,
+    "strategy"     text                     not null,
+    "chain_id"     bigint                   not null,
+    "id"           uuid                     not null default gen_random_uuid()
 );
 
 
-alter table "public"."fraction_sponsor_metadata" enable row level security;
+alter table "public"."fraction_sponsor_metadata"
+    enable row level security;
 
 CREATE UNIQUE INDEX fraction_sponsor_metadata_pkey ON public.fraction_sponsor_metadata USING btree (hypercert_id, fraction_id);
 
-alter table "public"."fraction_sponsor_metadata" add constraint "fraction_sponsor_metadata_pkey" PRIMARY KEY using index "fraction_sponsor_metadata_pkey";
+alter table "public"."fraction_sponsor_metadata"
+    add constraint "fraction_sponsor_metadata_pkey" PRIMARY KEY using index "fraction_sponsor_metadata_pkey";
 
 create policy "Enable insert access for all authenticated users"
     on "public"."fraction_sponsor_metadata"
