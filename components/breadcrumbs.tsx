0b540998e73f91d62304bb656d00badf814f0d2e
jipstavenuiter <jipstavenuiter@gmail.com>
2024-02-23 11:15:49 +0100

    (fix): use hypercert id as breadcrumb key

--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -5,18 +5,20 @@ export interface BreadcrumbEntry {
   name: string;
   onClick: () => void;
   isActive?: boolean;
+  id: string;
 }
 
 export interface BreadcrumbsProps {
   crumbs: BreadcrumbEntry[];
 }
+
 export const Breadcrumbs = ({ crumbs }: BreadcrumbsProps) => {
   return (
     <Breadcrumb separator={<BiChevronRight color="gray.500" />}>
       {crumbs.map((crumb) => (
         <BreadcrumbItem
           isCurrentPage={crumb.isActive}
-          key={crumb.name}
+          key={crumb.id}
           onClick={crumb.onClick}
         >
           <BreadcrumbLink>{crumb.name}</BreadcrumbLink>
