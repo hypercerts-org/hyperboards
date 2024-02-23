9086540bc561c0d4e2c20d7754f1136942198d75
jipstavenuiter <jipstavenuiter@gmail.com>
2024-02-23 11:17:38 +0100

    (fix): only fetch hyperboard data if id is given

--- hooks/useFetchHyperboardData.ts
+++ hooks/useFetchHyperboardData.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@/lib/supabase";
 
-export const useFetchHyperboardData = (hyperboardId: string) =>
+export const useFetchHyperboardData = (hyperboardId?: string) =>
   useQuery({
     queryKey: ["hyperboard", hyperboardId],
     queryFn: async () =>
@@ -10,8 +10,9 @@ export const useFetchHyperboardData = (hyperboardId: string) =>
         .select(
           "*, hyperboard_registries ( *, registries ( *, claims ( * ), blueprints ( * ) ) )",
         )
-        .eq("id", hyperboardId)
+        .eq("id", hyperboardId!)
         .single()
         .throwOnError()
         .then((x) => x.data),
+    enabled: !!hyperboardId,
   });
